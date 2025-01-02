07aeed44f80f23adffd3e673f876781c126112fd
pvukor23 <pvukor00@fesb.hr>
2025-01-02 17:24:33 +0100

    Update 1. zadatak.c

--- 1. zadatak/1. zadatak/1. zadatak.c
+++ 1. zadatak/1. zadatak/1. zadatak.c
@@ -20,7 +20,7 @@
 #define MALLOC_ERROR -2	// Konstanta koja označava grešku pri alokaciji memorije koristeći malloc funkciju
 #define SCANF_ERROR -3	// Konstanta koja označava grešku pri čitanju podataka iz datoteke koristeći fscanf
 
-typedef struct _Student {	// Definicija strukture za studenta
+typedef struct _student {	// Definicija strukture za studenta
 	char name[MAX_LENGTH];	// Polje za ime studenta (do 128 znakova)
 	char surname[MAX_LENGTH];	// Polje za prezime studenta (do 128 znakova)
 	double points;	// Bodovi studenta na kolokviju
