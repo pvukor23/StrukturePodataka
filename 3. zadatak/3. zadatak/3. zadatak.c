bea6af34bfaa2d1b6eb0d9ab07de503540d700e0
pvukor23 <pvukor00@fesb.hr>
2025-02-11 00:23:58 +0100

    Update 3. zadatak.c

--- 3. zadatak/3. zadatak/3. zadatak.c
+++ 3. zadatak/3. zadatak/3. zadatak.c
@@ -73,7 +73,7 @@ int readListFromFile(Position head);	// Funkcija koja čita cijelu listu osoba i
 
 int main()
 {
-	Person head = { .name = {0}, .surname = {0}, .birthYear = 0, .next = NULL };	// Inicijalizacija čvora liste (head) s praznim podacima; ovo je početni čvor koji služi kao glava liste
+	Person head = { .name = { 0 }, .surname = { 0 }, .birthYear = 0, .next = NULL };	// Inicijalizacija čvora liste (head) s praznim podacima; ovo je početni čvor koji služi kao glava liste
 
 	menu(&head);	// Pozivanje funkcije menu koja omogućuje korisniku upravljanje povezanim popisom osoba
 
